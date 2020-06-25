db061a4e30f7caca9e52262210b0ff3b289c9594
dwinetralizme <dwinetralizme@gmail.com>
2020-06-25 22:45:36 +0700

    Add files via upload
    initialis

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import Layout from './component/Layout'
+import './index.css'
+
+function App() {
+  return (
+      <Layout title="Chat App"/>
+  );
+}
+
+export default App;
